951e0d4ce5cfb35698672eb92eace0ba2dd559f2
Evgeny Kravets <kravets@netcube.ru>
2024-11-18 15:25:52 +0300

    add user repository

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -3,26 +3,33 @@ import type { Prisma, User } from '@prisma/client';
 import { prisma } from '#root/prisma/index.js';
 
 class UserRepository {
-  private prisma: PrismaClientX;
+  private storage: PrismaClientX;
 
-  constructor(prismaClient: PrismaClientX) {
-    this.prisma = prismaClient;
+  constructor(storageClient: PrismaClientX) {
+    this.storage = storageClient;
   }
 
   async findById(id: number): Promise<User | null> {
-    return this.prisma.user.findUnique({
+    return this.storage.user.findUnique({
       where: { id },
     });
   }
 
   async findByTelegramId(telegramId: number | undefined): Promise<User | null> {
-    return this.prisma.user.findUnique({
+    return this.storage.user.findUnique({
       where: { telegramId },
     });
   }
 
+  async updateByTelegramId(telegramId: number | undefined, data: Prisma.UserUpdateInput): Promise<User> {
+    return this.storage.user.update({
+      where: { telegramId },
+      data,
+    });
+  }
+
   async create(data: Prisma.UserCreateInput): Promise<User> {
-    return this.prisma.user.create({
+    return this.storage.user.create({
       data,
     });
   }
