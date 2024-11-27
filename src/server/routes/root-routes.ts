8c69497c8014581c19be1bf1081cf0d19d16fee4
Evgeny Kravets <kravets@netcube.ru>
2024-11-27 10:57:49 +0300

    rename root and auth routes

--- src/server/routes/root-routes.ts
+++ src/server/routes/root-routes.ts
@@ -2,7 +2,7 @@ import type { FastifyPluginAsync } from 'fastify';
 
 const rootRoute: FastifyPluginAsync = async (server) => {
   server.get('/', async () => {
-    return { hello: 'world' };
+    return { status: 'OK' };
   });
 };
 
