c30c8a329bdcb8c12924e89cec671fb9d3ad2349
Evgeny Kravets <kravets@netcube.ru>
2024-12-15 18:04:02 +0300

    add user service

---
+++ src/services/user.service.ts
@@ -0,0 +1,38 @@
+import { userRepository } from '#root/repositories/user.repository.js';
+import { wordRepository } from '#root/repositories/word.repository.js';
+import { wordProgressRepository } from '#root/repositories/word-progress.repository.js';
+
+export async function addCollectionToUser(userId: number, collectionId: number): Promise<void> {
+  // add collection to user
+  await userRepository.updateByTelegramId(userId, {
+    selectedCollections: {
+      connect: { id: collectionId },
+    },
+  });
+
+  // get all word ids in collection
+  const wordIds = await wordRepository.findIdsByCollectionId(collectionId);
+
+  // create word progress for each word
+  if (wordIds && wordIds.length > 0) {
+    const wordProgressData = wordIds.map(wordId => ({ userId, wordId, status: 'todo' }));
+    await wordProgressRepository.createMany(wordProgressData);
+  }
+}
+
+export async function removeCollectionFromUser(userId: number, collectionId: number) {
+  // remove collection from user
+  await userRepository.updateByTelegramId(userId, {
+    selectedCollections: {
+      disconnect: { id: collectionId },
+    },
+  });
+
+  // get all word ids in collection
+  const wordIds = await wordRepository.findIdsByCollectionId(collectionId);
+
+  // update statuses for selected word ids
+  if (wordIds && wordIds.length > 0) {
+    await wordProgressRepository.updateManyByUser(userId, wordIds, { status: 'pause' });
+  }
+}
