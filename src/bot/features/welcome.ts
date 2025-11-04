951a5665481622cfa0b8ace74342baa52686b1fc
Evgeny Kravets <kravets@netcube.ru>
2025-11-04 13:39:55 +0300

    enhance welcome command with formatted messages and onboarding menu

--- src/bot/features/welcome.ts
+++ src/bot/features/welcome.ts
@@ -1,6 +1,8 @@
 import type { Context } from '#root/bot/context.js';
 import { GREETING_CONVERSATION } from '#root/bot/conversations/index.js';
 import { logHandle } from '#root/bot/helpers/logging.js';
+import { onboardingMenu } from '#root/bot/menus/onboarding.js';
+import { b, fmt, i } from '@grammyjs/parse-mode';
 import { Composer } from 'grammy';
 
 const composer = new Composer<Context>();
@@ -8,8 +10,12 @@ const composer = new Composer<Context>();
 const feature = composer.chatType('private');
 
 feature.command('start', logHandle('command-start'), async (ctx) => {
+  const messageFmt = fmt`
+👋 ${ctx.t('welcome')}
+${ctx.t('welcome-message1')}
+${ctx.t('welcome-message2')}
+`;
   const userId = ctx.from.id;
-
   let user = await ctx.prisma.user.findUnique({
     where: { telegramId: userId },
   });
@@ -23,7 +29,11 @@ feature.command('start', logHandle('command-start'), async (ctx) => {
       },
     });
   }
-  return ctx.reply(ctx.t('welcome'));
+
+  await ctx.reply(messageFmt.text, { entities: messageFmt.entities });
+  await ctx.reply(ctx.t('onboarding-start'), {
+    reply_markup: onboardingMenu,
+  });
 });
 
 feature.command('greeting', logHandle('command-greeting'), (ctx) => {
